7f46a52b77cbfa31279014b7036be0945c3e2bf9
nanami <bethaniebell8474@gmail.com>
2026-09-05 03:31:16 -0700

    优化模型界面

--- src/admin/pages/secrets/models/models.component.ts
+++ src/admin/pages/secrets/models/models.component.ts
@@ -1,25 +1,47 @@
 import { Component } from '@angular/core';
-import {NzSkeletonComponent} from "ng-zorro-antd/skeleton";
-import {RouterLink} from "@angular/router";
-import {NzModalModule} from "ng-zorro-antd/modal";
-import {NzPopconfirmDirective} from "ng-zorro-antd/popconfirm";
-import {NzTooltipDirective} from "ng-zorro-antd/tooltip";
-import {NzMessageService} from "ng-zorro-antd/message";
-import {EditModelComponent} from "./edit-model/edit-model.component";
-import {NewModelComponent} from "./new-model/new-model.component";
-import {Model} from "../../../models/keys";
-import {KeyCallService} from "../../../services";
-import {MultipleAddModelComponent} from "./multiple-add-model/multiple-add-model.component";
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzEmptyModule } from 'ng-zorro-antd/empty';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzPopconfirmDirective } from 'ng-zorro-antd/popconfirm';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzTagModule } from 'ng-zorro-antd/tag';
+import { NzTooltipDirective } from 'ng-zorro-antd/tooltip';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { EditModelComponent } from './edit-model/edit-model.component';
+import { NewModelComponent } from './new-model/new-model.component';
+import { MultipleAddModelComponent } from './multiple-add-model/multiple-add-model.component';
+import { Model } from '../../../models/keys';
+import { KeyCallService } from '../../../services';
 
 @Component({
   selector: 'app-models',
   standalone: true,
   imports: [
-    NzSkeletonComponent,
+    FormsModule,
     RouterLink,
+    NzButtonModule,
+    NzCardModule,
+    NzDropDownModule,
+    NzEmptyModule,
+    NzIconModule,
+    NzInputModule,
     NzModalModule,
+    NzPageHeaderModule,
     NzPopconfirmDirective,
+    NzSpaceModule,
+    NzTableModule,
+    NzTagModule,
     NzTooltipDirective,
+    NzTypographyModule,
     EditModelComponent,
     NewModelComponent,
     MultipleAddModelComponent
@@ -29,60 +51,64 @@ import {MultipleAddModelComponent} from "./multiple-add-model/multiple-add-model
 })
 export class ModelsComponent {
   models: Model[] | undefined;
-  constructor(private message: NzMessageService, private call: KeyCallService) {
+  displayModels: Model[] = [];
+  searchText = '';
+
+  constructor(private message: NzMessageService, private call: KeyCallService) {}
+
+  newModelVisible = false;
+  editModelVisible = false;
+  multipleAddVisible = false;
+  editModel: Model | undefined;
+
+  ngOnInit() {
+    this.fetchModels();
   }
-  newModelVisible : boolean = false;
+
   refresh() {
     this.fetchModels(true);
   }
-  ngOnInit(){
-    this.fetchModels();
-  }
-  delete(roleId: number) {
-    this.call.deleteModel(roleId)
-      .subscribe({
-        next: msg=>{
-          this.message.success("删除成功");
-          this.fetchModels(true);
-        },
-        error: (err:any)=>{
-          this.message.error("删除错误");
-        }
-      })
-  }
-  fetchModels(refresh: boolean = false){
+
+  fetchModels(refresh: boolean = false) {
     this.call.getModelsWithKey().subscribe({
-      next: models=>{
+      next: models => {
         this.models = models;
-        if(refresh){
-          this.message.success("刷新成功");
+        this.filterModels();
+        if (refresh) {
+          this.message.success('刷新成功');
         }
       },
-      error:(err: any) => {
-        this.message.error("获取账户信息失败")
+      error: (err: any) => {
+        this.models = [];
+        this.filterModels();
+        this.message.error('获取模型信息失败');
       }
-    })
+    });
   }
 
-  tackleClose($event: boolean) {
-    if($event){
-      this.newModelVisible = false;
-      this.fetchModels(true);
+  filterModels() {
+    const keyword = this.searchText.trim().toLowerCase();
+    if (!keyword) {
+      this.displayModels = this.models ?? [];
+      return;
     }
+    this.displayModels = (this.models ?? []).filter(
+      model =>
+        model.name?.toLowerCase().includes(keyword) ||
+        model.modelValue?.toLowerCase().includes(keyword)
+    );
   }
-  multipleAddModalClose($event: boolean) {
-    if($event){
-      this.multipleAddVisible = false;
-      this.fetchModels(true);
-    }
-  }
-  editModelVisible: boolean = false;
-  editModel: Model | undefined;
-  tackleEditClose($event: boolean) {
-    if($event){
-      this.editModelVisible = false;
-      this.fetchModels(true);
-    }
+
+  delete(roleId: number) {
+    this.call.deleteModel(roleId).subscribe({
+      next: () => {
+        this.message.success('删除成功');
+        this.fetchModels(true);
+      },
+      error: () => {
+        this.message.error('删除错误');
+      }
+    });
   }
 
   editAModel(model: Model) {
@@ -92,24 +118,44 @@ export class ModelsComponent {
 
   disableModel(modelId: number | undefined) {
     this.call.disableModel(modelId!).subscribe({
-      next: (msg:any)=>{
-        this.message.success(msg.msg)
+      next: (msg: any) => {
+        this.message.success(msg.msg);
       },
       error: err => {
-        this.message.error(err.error)
+        this.message.error(err.error);
       }
-    })
+    });
   }
 
   enableModel(modelId: number | undefined) {
     this.call.enableModel(modelId!).subscribe({
-      next: (msg:any)=>{
-        this.message.success(msg.msg)
+      next: (msg: any) => {
+        this.message.success(msg.msg);
       },
       error: err => {
-        this.message.error(err.error)
+        this.message.error(err.error);
       }
-    })
+    });
   }
-  multipleAddVisible: boolean = false;
+
+  tackleClose($event: boolean) {
+    if ($event) {
+      this.newModelVisible = false;
+      this.fetchModels(true);
+    }
+  }
+
+  multipleAddModalClose($event: boolean) {
+    if ($event) {
+      this.multipleAddVisible = false;
+      this.fetchModels(true);
+    }
+  }
+
+  tackleEditClose($event: boolean) {
+    if ($event) {
+      this.editModelVisible = false;
+      this.fetchModels(true);
+    }
+  }
 }
